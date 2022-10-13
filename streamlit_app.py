82a622ffd59abb30b64712d98eac4fdaa1a38509
Hellsice <46599932+Hellsice@users.noreply.github.com>
2022-10-13 19:46:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,38 +37,21 @@ import seaborn as sns
 
 # In[3]:
 
+st.set_page_config(page_title="2022-2023 sem-1 Case 3: Dashboard", page_icon="👩‍🎓", layout = "wide", initial_sidebar_state="expanded")
+# st.title('2022-2023 sem-1 Case 3: Dashboard')
 
-st.title('2022-2023 sem-1 Case 3: Dashboard')
-st.header('Elektrisch mobiliteit en laadpalen')
-st.subheader(' Team 7: Olger, Bart, Annika, Estelle') 
+
 ##### Inleidende tekst 
-st.markdown('In dit dashboard hebben wij gebruik gemaakt van de datasets OpenChargeMap, laadpaalgebruik en aantallen elektrische auto’s.')
-st.markdown('Hiermee hebben wij verbanden kunnen leggen over de toename van elektrische autos, tijd dat elektrische auto’s aan de laadpaal zitten.')
+
+st.sidebar.title('Navigatie')
+pages = st.sidebar.radio('paginas', options=['Home',"Elektrische auto's", 'Chargemap', 'Laadpaaldata', 'Voorspelling'], label_visibility='hidden')
 
 
 # In[4]:
 
-
-st.subheader('Aantallen elektrische autos')
 df = pd.read_csv('voertuigen.csv')
 df = df.drop(columns=['Unnamed: 0'])
 
-
-st.markdown('De data over het onderwerp aantal elektrische autos is afkomstig van de website opendata.rdw')
-st.markdown('Hieruit zijn er twee datasets gedownload voor gebruik. De ene over elektrische autos en ander over brandstof gebruik.')
-st.markdown('Deze twee datasets zijn samengevoegd tot de nieuwe opgeschoonde dataset: "Open bezine"')
-st.markdown('Doormiddel van functie zijn er extra kolomen aan toegevoegd.')
-st.markdown('Zie hier dataset hieronder')
-
-st.dataframe(df)
-
-
-# In[5]:
-
-
-st.markdown('Vanuit deze "Open bezine" dataset zijn er visualisaties gemaakt.')
-st.markdown('Figuur hieronder bevat een cumulatieve lijndiagram van het aantal voertuigen per maand per brandstof categorie.')
-st.markdown('Zoals te zien blijkt het aantal elektische voertuigen in de loop van de jaren meer toeneemt dan over brandstog categorien')
 
 
 ### grafiek 1 maken:
@@ -81,7 +64,6 @@ fig1.update_layout(
     xaxis_title="Per maand door de jaren heen",
     yaxis_title="Aantal")
 
-st.plotly_chart(fig1)
 
 
 # In[6]:
@@ -90,9 +72,6 @@ st.plotly_chart(fig1)
 ##Dataset met alleen elektrische auto's : df_elek
 df_elek = df[(df['Brandstof omschrijving']=='Elektriciteit')]
 
-st.markdown('Figuur hieronder bevat gegevens over de aanschaf elekrische autos over de jaren heen weer')
-st.markdown('Door onder aan het figuur de slider te gebruiken kan er gezien worden welke auto merk in dat jaar erbij is gekomen')
-st.markdown('Weer valt er op te merken dat er veel meer elektrische autos in de laatste jaren toeneemt')
 ## Figuur 2: Aanschaf elektishce autos over de tijd
 fig2 = px.bar(data_frame=df_elek,   
                  x='Merk',  
@@ -110,13 +89,11 @@ fig2.update_layout(
     xaxis_title="Jaar",
     yaxis_title="Aantal")
 
-st.plotly_chart(fig2)
 
 
 # In[7]:
 
 
-st.subheader('Chargemap') 
 
 # maxresults=10000
 # url_chargemap = ("https://api.openchargemap.io/v3/poi?key=a386a50f-1e5d-4021-baaf-868394bc33e9/?output=json&countrycode=NL&maxresults="+str(maxresults))
@@ -134,8 +111,6 @@ st.subheader('Chargemap')
 # df_chargemap = df_chargemap[df_chargemap['AddressInfo.Postcode'].notna()]
 merged = pd.read_csv('merged.csv')
 df_chargemap = pd.read_csv('Chargemap data.csv')
-st.dataframe(merged)
-st.dataframe(df_chargemap)
 
 # df_chargemap = df_chargemap.merge(merged, left_on='AddressInfo.Postcode', right_on='PC6', how='left')
 
@@ -159,10 +134,8 @@ for i in merged['Gemeentenaam'].unique():
             globals()['%s' %i].add_child(folium.Marker(location=[row['AddressInfo.Latitude'], row['AddressInfo.Longitude']],
                                                        popup=row['AddressInfo.AddressLine1'])).add_to(map)
 folium.LayerControl(position='bottomleft', collapsed=False).add_to(map)
-st_data = folium_static(map)
 
 
-st.markdown('Cluster map')
 mapcluster = folium.Map(location = [52.2129919, 5.2793703], zoom_start=7, tiles=None)
 base_map = folium.FeatureGroup(name='Basemap', overlay=True, control=False)
 folium.TileLayer(tiles='OpenStreetMap').add_to(base_map)
@@ -184,10 +157,8 @@ for i in merged['Gemeentenaam'].unique():
                                                        popup=row['AddressInfo.AddressLine1'])).add_to(mapcluster)
 
 folium.LayerControl(position='bottomleft', collapsed=False).add_to(mapcluster)
-st_data = folium_static(mapcluster)
 
 
-st.markdown('Cluster and marker map')
 map2 = folium.Map(location = [52.2129919, 5.2793703], zoom_start=7, tiles=None)
 base_map = folium.FeatureGroup(name='Basemap', overlay=True, control=False)
 folium.TileLayer(tiles='OpenStreetMap').add_to(base_map)
@@ -208,19 +179,9 @@ for i in merged['Gemeentenaam'].unique():
             globals()['%s' %i].add_child(folium.Marker(location=[row['AddressInfo.Latitude'], row['AddressInfo.Longitude']],
                                                        popup=row['AddressInfo.AddressLine1'])).add_to(map2)
 folium.LayerControl(position='bottomleft', collapsed=False).add_to(map2)
-st_data = folium_static(map2)
 # In[10]:
 
 
-st.subheader('Laadpaaldata') 
-st.markdown("Om de date op te schonen en overzichtelijk te maken hebben we de volgende bewerkingen uitgevoerd:")
-st.markdown("Allereerst hebben we alle NA-waardes verwijderd.")
-st.markdown("Vervolgens hebben we alle datums omgezet naar datetime format.")
-st.markdown("Toen hebben we gegekeken of de aangesloten uren volgens de Ended-Started time wel overeen kwamen,")
-st.markdown("hierbij waren er 5 erg afwijkend, dus hebben we deze verwijderd.")
-st.markdown("Hevige 'uitschieters' hebben we verwijderd om de meest voorkomende oplaadtijden te kunnen plotten voor een beter overzicht")
-st.markdown("De gemiddelde en mediaan zijn berekend om te implementeren in de volgende figuren.")
-st.markdown("")
 
 laadpaaldata = pd.read_csv('laadpaaldata.csv')
 # print(laadpaaldata.isna().sum().sum()) # Geen NaN waardes te vinden in deze dataset
@@ -256,7 +217,6 @@ laadpaaldata = pd.read_csv('laadpaaldata.csv')
 # laadpaaldata = laadpaaldata[(laadpaaldata["ChargeTime"] < q_hi_charge) & (laadpaaldata["ChargeTime"] > q_low_charge)]
 
 # print(laadpaaldata.head())
-st.dataframe(laadpaaldata)
 
 # Dubbel check of de uitschieters weg zijn
 # fig = px.box(laadpaaldata, x="ChargeTime")
@@ -269,9 +229,6 @@ mediaan = round(laadpaaldata['ChargeTime'].median(), 4)
 # print('Gemiddelde: ' +str(int(gemiddelde))+ " uur en " +str(round((gemiddelde-int(gemiddelde))*60)) +" minuten")
 # print('Mediaan: ' +str(int(mediaan))+ " uur en " +str(round((mediaan-int(mediaan))*60)) +" minuten")
 
-st.markdown('In onderstaand figuur is de verdeling te zien van de tijd dat een auto oplaadt.')
-st.markdown('Het gemiddelde en de mediaan verschillen weinig. Wel is er duidelijk te zien dat zich twee toppen vormen,')
-st.markdown("een bimodale normale verdeling waarschijnlijk. De meeste auto's laden iets meer dan 2 uur op.")
 
 fig3 = px.histogram(laadpaaldata, x="ChargeTime", nbins=40, text_auto=True,
             title='Aantal laadpalen per oplaadtijd',
@@ -320,15 +277,12 @@ fig3.add_annotation(x=5.93, y=610,
             opacity=0.8)
 
 fig3.update_layout(yaxis_title="Aantal laadpalen")
-st.plotly_chart(fig3)
 
 # Groeperen op maand en de totale laad en connected time berekenen --> verwerken in een barplot
 laadtijd_per_maand = laadpaaldata.groupby('Maand')['ChargeTime', "ConnectedTime"].sum().reset_index()
 laadtijd_per_maand['Maand'] = laadtijd_per_maand['Maand'].astype(int)
 laadtijd_per_maand['Maand'] = laadtijd_per_maand['Maand'].apply(lambda x: calendar.month_abbr[x])
 
-st.markdown('In onderstaand figuur is een duidelijk verschil te zien in de tijd van aansluiten en daadwerkelijk opladen.')
-st.markdown("De auto's staan in totaal vaak twee keer zo lang aangesloten als dat ze aan het opladen zijn.")
 
 fig4 = go.Figure()
 fig4.add_trace(go.Bar(x=laadtijd_per_maand["Maand"], y=laadtijd_per_maand["ChargeTime"], name="Oplaadtijd"))
@@ -353,22 +307,11 @@ fig4.update_layout(yaxis_title="Totale tijd in uren",
 ))
                   ])
 
-st.plotly_chart(fig4)
 
 
 # In[13]:
 
 
-st.subheader("Voorspelling aantal elektrische auto's") 
-st.markdown("Doormiddel van de variabelen in de datasets is er een voorspelling gedaan over het aantal elektrische auto's.")
-st.markdown('Dit hebben wij kunnen doen door bepaalde gegevens te voorspellen en uit te zetten over de jaren later')
-st.markdown("Uit de figuur hieronder blijkt dat het aantal elektrische auto's tot met 2030 zal toenemen'")
-st.markdown('De blauwe bolletjes geven de echte waardes aan in de jaren die al gegeven zijn')
-st.markdown('De blauwe lijn geeft het gemiddelde aan door de blauwe bolletjes')
-st.markdown('Om te voorspellen wat er in de jaren na de blauwe bolletjes gebeurt,')
-st.markdown('zijn er "predictions" gemaakt die zijn gebaseerd op de gegeven punten')
-st.markdown('De rode bolletjes vormen de voorspelling in de toekomst')
-st.markdown("Hieruit zal blijken dat de aanschaf van elektrische auto's toeneemt in de toekomst")
 
 # Create sqrt_dist_to_mrt_m
 
@@ -399,11 +342,62 @@ ax2 = sns.scatterplot(x="jaar_tenaamstelling",
 ax1.set(xlabel='Jaar', ylabel="Aantal elektrische auto's")
 ax1.set(title="Aantal elektrische auto's per jaar en voorspelling aankomende jaren")
 
-st.pyplot(fig5)
 
-
-# In[ ]:
-
-
-
-
+if pages == 'Home':
+    st.header('Elektrisch mobiliteit en laadpalen')
+    st.subheader(' Team 7: Olger, Bart, Annika, Estelle')
+    st.markdown('In dit dashboard hebben wij gebruik gemaakt van de datasets OpenChargeMap, laadpaalgebruik en aantallen elektrische auto’s.')
+    st.markdown('Hiermee hebben wij verbanden kunnen leggen over de toename van elektrische autos, tijd dat elektrische auto’s aan de laadpaal zitten.')
+elif pages == "Elektrische auto's":
+    st.subheader('Aantallen elektrische autos')
+    st.markdown('De data over het onderwerp aantal elektrische autos is afkomstig van de website opendata.rdw')
+    st.markdown('Hieruit zijn er twee datasets gedownload voor gebruik. De ene over elektrische autos en ander over brandstof gebruik.')
+    st.markdown('Deze twee datasets zijn samengevoegd tot de nieuwe opgeschoonde dataset: "Open bezine"')
+    st.markdown('Doormiddel van functie zijn er extra kolomen aan toegevoegd.')
+    st.markdown('Zie hier dataset hieronder')
+    st.dataframe(df)
+    st.markdown('Vanuit deze "Open bezine" dataset zijn er visualisaties gemaakt.')
+    st.markdown('Figuur hieronder bevat een cumulatieve lijndiagram van het aantal voertuigen per maand per brandstof categorie.')
+    st.markdown('Zoals te zien blijkt het aantal elektische voertuigen in de loop van de jaren meer toeneemt dan over brandstog categorien')
+    st.plotly_chart(fig1)
+    st.markdown('Figuur hieronder bevat gegevens over de aanschaf elekrische autos over de jaren heen weer')
+    st.markdown('Door onder aan het figuur de slider te gebruiken kan er gezien worden welke auto merk in dat jaar erbij is gekomen')
+    st.markdown('Weer valt er op te merken dat er veel meer elektrische autos in de laatste jaren toeneemt')
+    st.plotly_chart(fig2)
+elif pages == 'Chargemap':
+    st.subheader('Chargemap') 
+    st.markdown('Map van laadpalen per gemeente, aangegeven door markers.')
+    st_data = folium_static(map)
+    st.markdown('Map van de laadpalen per gemeente of allemaal')
+    st_data = folium_static(mapcluster)
+    st.markdown('Een combinatie van de eerste twee mappen. Alle laadpalen weergeven met clusters of weergeven met markers per gemeente.')
+    st_data = folium_static(map2)
+elif pages == 'Laadpaaldata':
+    st.subheader('Laadpaaldata') 
+    st.markdown("Om de date op te schonen en overzichtelijk te maken hebben we de volgende bewerkingen uitgevoerd:")
+    st.markdown("Allereerst hebben we alle NA-waardes verwijderd.")
+    st.markdown("Vervolgens hebben we alle datums omgezet naar datetime format.")
+    st.markdown("Toen hebben we gegekeken of de aangesloten uren volgens de Ended-Started time wel overeen kwamen,")
+    st.markdown("hierbij waren er 5 erg afwijkend, dus hebben we deze verwijderd.")
+    st.markdown("Hevige 'uitschieters' hebben we verwijderd om de meest voorkomende oplaadtijden te kunnen plotten voor een beter overzicht")
+    st.markdown("De gemiddelde en mediaan zijn berekend om te implementeren in de volgende figuren.")
+    st.dataframe(laadpaaldata)
+    st.markdown('In onderstaand figuur is de verdeling te zien van de tijd dat een auto oplaadt.')
+    st.markdown('Het gemiddelde en de mediaan verschillen weinig. Wel is er duidelijk te zien dat zich twee toppen vormen,')
+    st.markdown("een bimodale normale verdeling waarschijnlijk. De meeste auto's laden iets meer dan 2 uur op.")
+    st.plotly_chart(fig3)
+    st.markdown('In onderstaand figuur is een duidelijk verschil te zien in de tijd van aansluiten en daadwerkelijk opladen.')
+    st.markdown("De auto's staan in totaal vaak twee keer zo lang aangesloten als dat ze aan het opladen zijn.")
+    st.plotly_chart(fig4)
+elif pages == 'Voorspelling':
+    st.subheader("Voorspelling aantal elektrische auto's") 
+    st.markdown("Doormiddel van de variabelen in de datasets is er een voorspelling gedaan over het aantal elektrische auto's.")
+    st.markdown('Dit hebben wij kunnen doen door bepaalde gegevens te voorspellen en uit te zetten over de jaren later')
+    st.markdown("Uit de figuur hieronder blijkt dat het aantal elektrische auto's tot met 2030 zal toenemen'")
+    st.markdown('De blauwe bolletjes geven de echte waardes aan in de jaren die al gegeven zijn')
+    st.markdown('De blauwe lijn geeft het gemiddelde aan door de blauwe bolletjes')
+    st.markdown('Om te voorspellen wat er in de jaren na de blauwe bolletjes gebeurt,')
+    st.markdown('zijn er "predictions" gemaakt die zijn gebaseerd op de gegeven punten')
+    st.markdown('De rode bolletjes vormen de voorspelling in de toekomst')
+    st.markdown("Hieruit zal blijken dat de aanschaf van elektrische auto's toeneemt in de toekomst")
+    st.pyplot(fig5)
