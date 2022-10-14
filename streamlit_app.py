b97f54adbf2e25f87a2286e815cdbad7034d2dff
Hellsice <46599932+Hellsice@users.noreply.github.com>
2022-10-14 10:29:10 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,6 +14,7 @@ import plotly.graph_objects as go
 from statsmodels.formula.api import ols
 import matplotlib.pyplot as plt
 import seaborn as sns
+import geopandas as gpd
 
 df = pd.read_csv('voertuigen.csv')
 df = df.drop(columns=['Unnamed: 0'])
